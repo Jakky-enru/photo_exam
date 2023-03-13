206c6b5431372fc70249e026166638f39c10a102
下津浦良 <shimotsuuraryo@shitatsuuraryounoMacBook-Air.local>
2023-03-13 14:12:37 +0900

    課題提出

---
+++ config/application.rb
@@ -0,0 +1,29 @@
+require_relative "boot"
+
+require "rails/all"
+
+Bundler.require(*Rails.groups)
+
+module PhotoApp
+  class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :ja
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.1
+
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root to: 'sessions#new'
+  resources :favorites, only: [:create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show, :edit, :update]
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+end
+
